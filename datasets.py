33e349b737ae2c67ba29d5d9398bb153796daba1
Shimada Yuki <e175750@ie.u-ryukyu.ac.jp>
2019-11-12 16:34:14 +0900

    3rd commmit

--- datasets.py
+++ datasets.py
@@ -10,11 +10,11 @@ def load_nonlinear_example1():
     Y = np.array([4.0,0.0,3.0,2.0])
     return X,Y
 
-def polynominal2_features(input):
+def polynomial2_features(input):
     """
     >>> import datasets
     >>> X,Y = datasets.load_nonlinear_example1()
-    >>> ex_X = datasets.polynominal2_features(X)
+    >>> ex_X = datasets.polynomial2_features(X)
     >>> ex_X
     array([[ 1.  ,  0.  ,  0.  ],
            [ 1.  ,  2.  ,  4.  ],
@@ -26,5 +26,22 @@ def polynominal2_features(input):
     """
     poly2 = input[:,1:]**2
     return np.c_[input,poly2]
+
+def polynomial3_features(input):
+    """
+    >>> import datasets
+    >>> X,Y = datasets.load_nonlinear_example1()
+    >>> ex_X = datasets.polynomial3_features(X)
+    >>> ex_X
+    array([[ 1.   ,  0.   ,  0.   ,  0.   ],
+           [ 1.   ,  2.   ,  4.   ,  8.   ],
+           [ 1.   ,  3.9  , 15.21 , 59.319],
+           [ 1.   ,  4.   , 16.   , 64.   ]])
+    >>> Y
+    array([4., 0., 3., 2.])
+    """
+    poly2 = input[:,1:]**2
+    poly3 = input[:,1:]**3
+    return np.c_[input, poly2, poly3]
 
 
